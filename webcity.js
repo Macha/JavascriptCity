a82799cbd1952f8ddf6910da2f1cff2f1cffd6c7
Macha <Macha.hack@gmail.com>
2009-07-26 18:03:24 +0100

    initial commit

---
+++ webcity.js
@@ -0,0 +1,189 @@
+/*
+ *  Javascript City
+ *
+ *  Copyright (C) 2009 Macha <macha.hack@gmail.com>
+ *
+ *  This file is part of Javascript City (JSC).
+ *
+ *  JSC is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JSC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with JSC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+$(document).ready(function() {
+ 	jsc.init();
+	setTimeout("jsc.update()", jsc.core.day_length);
+});
+/**
+ * This object contains all the data in use by the program.
+ */
+jsc = {}; // Initialise Core jsc object
+// Initialise JSC subobjects
+/**
+ * Core data. Most of these are set once.
+ */
+jsc.core = {};
+/** 
+ * The clock. Used to store timekeeping data and functions
+ */
+jsc.clock = {};
+/**
+ * Updateable data. This is used to keep track of data in use by multiple tools and processes.
+ */
+jsc.data = {};
+
+/**
+ * Stores user changable game settings.
+ */
+jsc.settings = {};
+/**
+ * Core game function for starting a new game
+ */
+jsc.init = function() {
+	
+	jsc.core.paused = false;
+	jsc.core.day_length = 2500;
+	jsc.core.grid_size = 200;
+	
+	jsc.data.cash = 10000;
+	jsc.data.citizens = 0;
+	jsc.data.jobs = 0;
+	
+	jsc.data.currentTool = null;
+	
+	jsc.clock.day = 1;
+	jsc.clock.month = 1;
+	jsc.clock.year = 1900;
+	jsc.clock.daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+	
+	jsc.settings.currency = "$";
+	
+	// Initialise cells to null
+	jsc.data.cells = [];
+	for(var x = 0; x < jsc.core.grid_size; x++) {
+		jsc.data.cells[x] = [];
+		for(var y = 0; y < jsc.core.grid_size; y++) {
+			jsc.data.cells[x][y] = {type: null, developed: false, powered: false, watered: false, hasTransport: false, wealth: 0, quality: 0};
+		}
+	}
+}
+/**
+ * Core updating function. Runs different update functions in a loop.
+ */
+jsc.update = function() {
+	if(!jsc.core.paused) {
+		jsc.clock.update();
+		if(jsc.clock.day == 1) { // First week run budget
+			jsc.core.doBudget();
+		}
+		if (jsc.clock.day % 7 == 0) {
+			for(var process in jsc.processes) {
+				jsc.processes[process].run();
+			}
+		}
+		jsc.ui.update();
+	}
+	setTimeout(jsc.update, jsc.core.day_length);
+};
+jsc.core.doBudget = function() {
+	return false; // TODO implement
+};
+/**
+ * Generates a random number.
+ */
+jsc.core.getRand = function() {
+	return Math.floor(Math.random() * 100);
+};
+/**
+ * Updates a property in a certain radius of the current cell.
+ * @param {Number} squares The maximum distance of cells to update
+ * @param {String} prop The property to change
+ * @param {Object} value The value to set
+ */
+jsc.core.updatePropAroundSelection = function(squares, prop, value) {
+	var scanStartX = jsc.data.selection.startX - squares;
+	if(scanStartX < 0) {
+		scanStartX = 0;
+	}
+	var scanStartY = jsc.data.selection.startY - squares;
+	if(scanStartY < 0) {
+		scanStartY = 0;
+	}
+	var scanEndX = jsc.data.selection.endX + squares;
+	if(scanEndX > 199) {
+		scanEndX = 199;
+	}
+	var scanEndY = jsc.data.selection.endY + squares;
+	if(scanEndY > 199) {
+		scanEndY = 199;
+	}
+	
+	for(x = scanStartX; x <= scanEndX; x++) {
+		for(y = scanStartY; y <= scanEndY; y++) {
+			jsc.data.cells[x][y][prop] = value;
+		}
+	}
+};
+/**
+ *  Updates game clock
+ */
+jsc.clock.update = function() {
+	jsc.clock.day++;
+	if(jsc.clock.day > jsc.clock.daysInMonth[jsc.clock.month - 1]) {
+		jsc.clock.day = 1;
+		jsc.clock.month++;
+		if(jsc.clock.month >= 12) { 
+			jsc.clock.month = 1;
+			jsc.clock.year++;
+		}
+	}
+};
+/**
+ * Gets the co-ordinates as an array for an object.
+ * @param {String} id The cells id (in the format cx-y)
+ */
+jsc.core.coordsFromId = function(id) {
+	var coord_array = id.split("-");
+	coord_array[0] = coord_array[0].replace("c", "");
+	return coord_array;
+};
+/**
+ * Prepares a selection to be looped by reversing X or Y axis as neccesary
+ * @param {Object} selection The selection to prepare. For the current selection, use jsc.data.selection
+ */
+jsc.core.prepareSelection = function(selection) {
+	var end_selection =  {startX: selection.startX, endX: selection.endX, startY: selection.startY, endY: selection.endY };
+	if(end_selection.startX > end_selection.endX) {
+		var tempX = end_selection.endX;
+		end_selection.endX = end_selection.startX;
+		end_selection.startX = tempX;
+	}
+	if(end_selection.startY > end_selection.endY) {
+		var tempY = end_selection.endY;
+		end_selection.endY = end_selection.startY;
+		end_selection.startY = tempY;
+	}
+	return end_selection;
+}
+jsc.core.pause = function() {
+	jsc.core.paused = !jsc.core.paused;
+};
+jsc.core.save = function() {
+	json = $.toJSON(jsc.data.cells);
+	alert(json);
+};
+jsc.core.changeCash = function(amount, show_at_cursor) {
+	show_at_cursor = show_at_cursor || false;
+	jsc.data.cash += amount;
+	if(show_at_cursor) {
+		jsc.ui.showCashChange(amount);
+	}
+};
---
+++ webcity.tools.js
@@ -0,0 +1,276 @@
+/*
+ *  Javascript City
+ *
+ *  Copyright (C) 2009 Macha <macha.hack@gmail.com>
+ *
+ *  This file is part of Javascript City (JSC).
+ *
+ *  JSC is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JSC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with JSC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+$(document).ready(function() {
+	for(var tool in jsc.tools) {
+		jsc.tools[tool].init();
+	}
+});
+/**
+ * Used to store the tool's functions.
+ */ 
+jsc.tools = {};
+/**
+ * Used to store the data used by tools. Each tool should have a sub property assigned to it.
+ */ 
+jsc.toolData = {};
+
+/**
+ * The residential tools for housing zones.
+ */
+jsc.tools.res = {};
+/**
+ * Initialises the residential tool for use.
+ */
+jsc.tools.res.init = function() {
+	jsc.toolData.res = {};
+	jsc.toolData.res.cost = 15;
+	jsc.toolData.res.removeCost = 10;
+	jsc.toolData.res.image = "images/res.png";
+};
+/**
+ * Runs the residential tool on the current selection (jsc.data.selection)
+ */
+jsc.tools.res.run = function() {
+	for(var x = jsc.data.selection.startX; x <= jsc.data.selection.endX; x++) {
+		if(jsc.data.cash < jsc.toolData.res.cost) {
+			break;
+		}
+		for(var y = jsc.data.selection.startY; y <= jsc.data.selection.endY; y++) {
+			if(jsc.data.cash < jsc.toolData.res.cost) {
+				break;
+			}
+			if(jsc.data.cells[x][y].type != null) {
+				jsc.tools[jsc.data.cells[x][y].type].remove(x, y);
+			}
+			jsc.data.cells[x][y].type = "res";
+			jsc.data.cash -= jsc.toolData.res.cost;
+			jsc.ui.addClassToCell("res", x, y);
+			jsc.ui.clearSelection();
+		}
+	}
+};
+/**
+ * Removes residential zone from a cell.
+ * @param {Number} x The x co-ordinate of the cell.
+ * @param {Number} y The y co-ordinate of the cell
+ */
+jsc.tools.res.remove = function(x, y) {
+	if(jsc.data.cells[x][y].developed) {
+		jsc.data.cash -= jsc.toolData.res.removeCost;
+	}
+	jsc.data.cells[x][y].type = null;
+	jsc.data.cells[x][y].developed = false;
+	jsc.ui.removeClassFromCell("res developed", x, y);
+};
+/**
+ * The industrial tools for factory zones.
+ */
+jsc.tools.ind = {};
+/**
+ * Initialises the industrial tool for use.
+ */
+jsc.tools.ind.init = function() {
+	jsc.toolData.ind = {};
+	jsc.toolData.ind.cost = 20;
+	jsc.toolData.ind.removeCost = 15;
+	jsc.toolData.ind.image = "images/ind.png";
+};
+/**
+ * Runs the industrial tool on the current selection (jsc.data.selection)
+ */
+jsc.tools.ind.run = function() {
+	for(var x = jsc.data.selection.startX; x <= jsc.data.selection.endX; x++) {
+		if(jsc.data.cash < jsc.toolData.ind.cost) {
+			break;
+		}
+		for(var y = jsc.data.selection.startY; y <= jsc.data.selection.endY; y++) {
+			if(jsc.data.cash < jsc.toolData.ind.cost) {
+				break;
+			}
+			if(jsc.data.cells[x][y].type != null) {
+				jsc.tools[jsc.data.cells[x][y].type].remove(x, y);
+			}
+			jsc.data.cells[x][y].type = "ind";
+			jsc.core.changeCash(-jsc.toolData.ind.cost);
+			jsc.ui.addClassToCell("ind", x, y);
+			jsc.ui.clearSelection();
+		}
+	}
+};
+/**
+ * Removes industrial zone from a cell.
+ * @param {Number} x The x co-ordinate of the cell.
+ * @param {Number} y The y co-ordinate of the cell
+ */
+jsc.tools.ind.remove = function(x, y) {
+	if(jsc.data.cells[x][y].developed) {
+		jsc.data.cash -= jsc.toolData.ind.removeCost;
+	}
+	jsc.data.cells[x][y].type = null;
+	jsc.data.cells[x][y].developed = false;
+	jsc.ui.removeClassFromCell("ind developed", x, y);
+};
+jsc.tools.com = {};
+jsc.tools.com.run = function() {
+	for(var x = jsc.data.selection.startX; x <= jsc.data.selection.endX; x++) {
+		if(jsc.data.cash < jsc.toolData.com.cost) {
+			break;
+		}
+		for(var y = jsc.data.selection.startY; y <= jsc.data.selection.endY; y++) {
+			if(jsc.data.cash < jsc.toolData.com.cost) {
+				break;
+			}
+			if(jsc.data.cells[x][y].type != null) {
+				jsc.tools[jsc.data.cells[x][y].type].remove(x, y);
+			}
+			jsc.data.cells[x][y].type = "com";
+			jsc.data.cash -= jsc.toolData.com.cost;
+			jsc.ui.addClassToCell("com", x, y);
+			jsc.ui.clearSelection();
+		}
+	}
+};
+jsc.tools.com.init = function() {
+	jsc.toolData.com = {};
+	jsc.toolData.com.cost = 20;
+	jsc.toolData.com.removeCost = 12;
+	jsc.toolData.com.image = "images/com.png";
+};
+jsc.tools.com.remove = function(x, y) {
+	if(jsc.data.cells[x][y].developed) {
+		jsc.data.cash -= jsc.toolData.com.removeCost;
+	}
+	jsc.data.cells[x][y].type = null;
+	jsc.data.cells[x][y].developed = false;
+	jsc.ui.removeClassFromCell("com developed", x, y);
+};
+
+jsc.tools.road = {};
+jsc.tools.road.init = function() {
+	jsc.toolData.road = {};
+	jsc.toolData.road.cost = 3;
+	jsc.toolData.road.removeCost = 4;
+	jsc.toolData.road.image = "images/road.png";
+};
+jsc.tools.road.run = function() {
+	for(var x = jsc.data.selection.startX; x <= jsc.data.selection.endX; x++) {
+		if(jsc.data.cash < jsc.toolData.road.cost) {
+			break;
+		}
+		for(var y = jsc.data.selection.startY; y <= jsc.data.selection.endY; y++) {
+			if(jsc.data.cash < jsc.toolData.road.cost) {
+				break;
+			}
+			if(jsc.data.cells[x][y].type != null) {
+				jsc.tools[jsc.data.cells[x][y].type].remove(x, y);
+			}
+			jsc.data.cells[x][y].type = "road";
+			jsc.data.cash -= jsc.toolData.road.cost;
+			jsc.ui.addClassToCell("road", x, y);
+		}
+	}
+	jsc.tools.road.correctType();
+	jsc.ui.clearSelection();
+	jsc.core.updatePropAroundSelection(3, "hasTransport", true);
+};
+jsc.tools.road.correctType = function() {
+	var selection = jsc.core.prepareSelection(jsc.data.selection);
+	
+	firstX = (selection.startX > 0) ? selection.startX - 1 : selection.startX;
+	lastX = (selection.endX < 199) ? selection.endX + 1 : selection.endX;
+	firstY = (selection.startY > 0) ? selection.startY - 1 : selection.startY;
+	lastY = (selection.endY < 199) ? selection.endY + 1 : selection.endY;
+
+	
+	
+	for(var x = firstX; x <= lastX; x++) { // Uses x declared above
+		for(var y = firstY; y <= lastY; y++) {
+			if(
+				(jsc.data.cells[x-1] == undefined || jsc.data.cells[x-1][y].type != "road" )
+				&& 
+				(jsc.data.cells[x+1] == undefined || jsc.data.cells[x+1][y].type != "road" )
+				&& 
+				jsc.data.cells[x][y].type == "road"
+			) {
+				jsc.ui.addClassToCell("horz", x, y);
+			}
+			else {
+				jsc.ui.removeClassFromCell("horz", x, y);
+			}
+			if(
+				(jsc.data.cells[x][y-1] == undefined || jsc.data.cells[x][y-1].type != "road")
+				&& 
+				(jsc.data.cells[x][y+1] == undefined || jsc.data.cells[x][y+1].type != "road")
+				&& 
+				jsc.data.cells[x][y].type == "road"
+			) {
+				jsc.ui.addClassToCell("vert", x, y);
+			}
+			else {
+				jsc.ui.removeClassFromCell("vert", x, y);
+			}
+		}
+	}
+};
+jsc.tools.road.remove = function(x, y) {
+	jsc.data.cash -= jsc.toolData.road.removeCost;
+	jsc.data.cells[x][y].type = null;
+	jsc.data.cells[x][y].developed = false;
+	jsc.ui.removeClassFromCell("road", x, y);
+};
+jsc.tools.coal = {};
+jsc.tools.coal.init = function() {
+	jsc.toolData.coal = {};
+	jsc.toolData.coal.cost = 100;
+	jsc.toolData.coal.removeCost = 40;
+	jsc.toolData.coal.powerAmount = 1000;
+	jsc.toolData.coal.image = "images/coal-power.png";
+};
+jsc.tools.coal.run = function() {
+	var selection = jsc.core.prepareSelection(jsc.data.selection);
+	var locX = selection.startX;
+	var locY = selection.startY;
+
+	jsc.data.cells[locX][locY].type = "coal";
+	jsc.data.cash -= jsc.toolData.coal.cost;
+	jsc.ui.addClassToCell("coal", locX, locY);
+	
+	jsc.data.powerSources.push({totalPower: jsc.toolData.coal.powerAmount, usedPower: 0, x: locX, y: locY});
+};
+jsc.tools.coal.remove = function(x, y) {
+	jsc.data.cash -= jsc.toolData.coal.removeCost;
+	jsc.data.cells[x][y].type = null;
+	jsc.data.cells[x][y].developed = false;
+	jsc.ui.removeClassFromCell("coal", x, y);
+};
+jsc.tools.query = {};
+jsc.tools.query.init = function() {
+	jsc.toolData.query = {};
+	jsc.toolData.query.image = "images/query.png";
+};
+jsc.tools.query.run = function() {
+	var selection = jsc.core.prepareSelection(jsc.data.selection);
+	var x = selection.startX;
+	var y = selection.startY;
+	
+	alert("Powered: " + jsc.data.cells[x][y].powered);
+};
+jsc.tools.query.remove = function() {};
