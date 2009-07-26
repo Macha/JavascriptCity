4b15f159600f1ec865ec44699cf6ef833c8c87b2
Macha <Macha.hack@gmail.com>
2009-07-26 10:54:51 -0700

    Fix syntax error (did not pay enough attention when pasting in GPL header.

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with JSC.  If not, see <http://www.gnu.org/licenses/>.
  */
- *?>
+?>
 <!DOCTYPE html>
 <html>
 	<head>
